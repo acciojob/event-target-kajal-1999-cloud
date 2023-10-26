f53f85206be372919de8b3c4ab4312aba307cbf4
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-10-26 17:08:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,32 @@ class EventTarget {
     }
   
     addEventListener(event, callback) {
-      
+      if (!this.listeners.has(event)) {
+        this.listeners.set(event, new Set());
+      }
+      const eventListeners = this.listeners.get(event);
+      if (!eventListeners.has(callback)) {
+        eventListeners.add(callback);
+      }
     }
   
     removeEventListener(event, callback) {
-      
+      if (this.listeners.has(event)) {
+        const eventListeners = this.listeners.get(event);
+        eventListeners.delete(callback);
+        if (eventListeners.size === 0) {
+          this.listeners.delete(event);
+        }
+      }
     }
   
     dispatchEvent(event) {
-    
+      if (this.listeners.has(event)) {
+        const eventListeners = this.listeners.get(event);
+        for (const callback of eventListeners) {
+          callback();
+        }
+      }
     }
-    
   }
   
